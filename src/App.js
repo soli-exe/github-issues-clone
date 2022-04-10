e72cb5f3634d3021ac20575a09fb44147c1a01ef
soli-exe <sja.exe@gmail.com>
2022-04-10 14:57:37 +0430

    Made pagination items and links responsive

--- src/App.js
+++ src/App.js
@@ -90,14 +90,14 @@ const App = () => {
         containerClassName='paginate-container'
         activeClassName='current'
         pageClassName="page-item"
-        previousClassName="page-item"
-        nextClassName="page-item"
+        previousClassName="page-item-btn"
+        nextClassName="page-item-btn"
         nextLinkClassName="page-link"
         previousLinkClassName="page-link"
         breakClassName="disabled-item"
         breakLinkClassName="disabled-link"
         breakLabel="..."
-        pageRangeDisplayed={5}
+        pageRangeDisplayed={window.screen.width > '768' ? 5 : 1}
         nextLabel="Next >"
         previousLabel="< Previous"
         pageCount={Math.ceil(pageCount)}
